11b2c7f22b980a794d96d16175bae0391edad149
*mkalouda* <munz.kalouda@gmail.com>
2021-03-11 20:56:53 -0500

    yeaaaaaa

--- src/testTheGit/git.java
+++ src/testTheGit/git.java
@@ -4,6 +4,15 @@ public class git {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("Man we are tired");
+		
+		/*
+		 * these are comment lines
+		 * 
+		 */
+		
+		System.out.println("WE ARE LEARNING GIT!");
 
 	}
 
